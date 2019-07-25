e9925b27f71511e90c5b32a814ac8e89767bb6de
Santiago Calvo <santiago@upshow.tv>
2019-07-25 16:19:20 -0300

    add todo

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,7 @@ var sectionHeight = function() {
 $(window).resize(sectionHeight);
 
 $(function() {
+  // @TODO Quitar Jquery
   $("section h1, section h2, section h3").each(function(){
     $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
     $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
